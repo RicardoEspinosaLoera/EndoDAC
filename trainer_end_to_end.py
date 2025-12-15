12c5d3354edbce26e6aed827895ec76badf0763c
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2025-12-15 09:58:57 -0600

    jpg

--- trainer_end_to_end.py
+++ trainer_end_to_end.py
@@ -136,7 +136,7 @@ class Trainer:
         train_filenames = readlines(fpath.format("train"))
         val_filenames = readlines(fpath.format("val"))
         test_filenames = readlines(fpath.format("test"))
-        img_ext = '.png'  
+        img_ext = '.jpg'  
 
         num_train_samples = len(train_filenames)
         self.num_total_steps = num_train_samples // self.opt.batch_size * self.opt.num_epochs
