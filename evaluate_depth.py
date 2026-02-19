89266076c9ac31f68e9960aec84271f054cb326d
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2026-02-18 23:25:13 -0600

    evaluate

--- evaluate_depth.py
+++ evaluate_depth.py
@@ -177,12 +177,10 @@ def evaluate(opt):
                 frame_id = "{:06d}".format(data['frame_id'][0])
             elif opt.eval_split == 'hamlyn' or opt.eval_split == 'c3vd':
                 gt_depth = gt_depths[i]
-                if gt_depth.ndim == 3:
-                    gt_depth = gt_depth[:, :, 0]  # Extract first channel if 3D
                 #sequence = str(np.array(data['sequence'][0]))
                 #keyframe = str(np.array(data['keyframe'][0]))
-                frame_id = "{:06d}".format(data['frame_id'][0])
-                #f_str = "{}{}".format(frame_index, self.img_ext)
+                #frame_id = "{:06d}".format(data['frame_id'][0])
+
             gt_height, gt_width = gt_depth.shape[:2]
             pred_disp = cv2.resize(pred_disp, (gt_width, gt_height))
             pred_depth = 1/pred_disp
@@ -193,7 +191,6 @@ def evaluate(opt):
                 vis_file_name = os.path.join(vis_dir, sequence + "_" +  keyframe + "_" + frame_id + ".png")
                 cv2.imwrite(vis_file_name, vis_pred_depth)
             
-            #print(pred_depth.shape, gt_depth.shape, mask.shape)
             pred_depth = pred_depth[mask]
             gt_depth = gt_depth[mask]
             
