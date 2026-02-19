eff24ba33d683bbf48169c33307410758b2f5a07
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2026-02-18 22:58:43 -0600

    endovis

--- options.py
+++ options.py
@@ -235,7 +235,7 @@ class MonodepthOptions:
                                  help="optional path to a .npy disparities file to evaluate")
         self.parser.add_argument("--eval_split",
                                  type=str,
-                                 default="hamlyn",
+                                 default="endovis",
                                  choices=[
                                     "hamlyn", "c3vd", "endovis"],
                                  help="which split to run eval on")
