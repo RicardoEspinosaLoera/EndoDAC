4c7d7fb8effc659e2b80a27f5ac5b98753a1b777
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2026-02-23 21:03:24 -0600

    gt_path

--- evaluate_depth_all.py
+++ evaluate_depth_all.py
@@ -175,8 +175,9 @@ def evaluate(opt):
                             pin_memory=True, drop_last=False)
 
     # Load ground truth
-    gt_path = os.path.join(splits_dir, opt.eval_split, "gt_depths.npz")
-    gt_depths = np.load(gt_path, fix_imports=True, encoding='latin1')["data"]
+    if opt.eval_split == 'endovis':
+        gt_path = os.path.join(splits_dir, opt.eval_split, "gt_depths.npz")
+        gt_depths = np.load(gt_path, fix_imports=True, encoding='latin1')["data"]
 
     print("-> Evaluating on {} split".format(opt.eval_split))
 
